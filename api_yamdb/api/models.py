35327a8daf2f444ba3587bbe636ea00b802f7b5d
Nikolai Volosiankov <wol87@ya.ru>
2024-10-29 10:32:15 +0100

    Add Review model

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)])
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_review',
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:50]
