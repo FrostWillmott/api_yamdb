25b0b3db9811188f7b27e087d6313abfcfb612f7
Nikolai Volosiankov <wol87@ya.ru>
2024-10-29 11:33:17 +0100

    Update ReviewViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,23 @@
-from rest_framework import mixins
 from rest_framework.generics import get_object_or_404
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Review, Title
 from .serializers import ReviewSerializer
 
 
-class ReviewViewSet(mixins.ListModelMixin,
-                    GenericViewSet):
-    queryset = Review.objects.all()
+class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)  # TODO add permissions
+
+    def _get_title(self):
+        return get_object_or_404(Title, id=self.kwargs['title_id'])
 
     def get_queryset(self):
-        title = get_object_or_404(Title, id=self.kwargs['title_id'])
+        title = self._get_title()
         return Review.objects.filter(title=title)
+
+    def perform_create(self, serializer):
+        title = self._get_title()
+        user = self.request.user
+        serializer.save(author=user, title=title)
