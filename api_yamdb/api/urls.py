eb0e7b250d01a6cfc722a243a61e72d08bb449ad
Nikolai Volosiankov <wol87@ya.ru>
2024-10-29 21:45:38 +0100

    Fix test_09_users_me_get

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,7 +6,6 @@ from api.views import SignupViewSet, TokenViewSet, UserProfileViewSet, \
 v1_router = DefaultRouter()
 v1_router.register('auth/signup', SignupViewSet, basename='signup')
 v1_router.register('auth/token', TokenViewSet, basename='token')
-v1_router.register('users/me', UserProfileViewSet, basename='user-profile')
 v1_router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
@@ -15,5 +14,6 @@ urlpatterns = [
     #     'patch': 'update',
     #     'delete': 'destroy'
     # }), name='user-detail'),
+    path('v1/users/me/', UserProfileViewSet.as_view({'get': 'retrieve'}), name='user-profile'),
     path('v1/', include(v1_router.urls)),
 ]
