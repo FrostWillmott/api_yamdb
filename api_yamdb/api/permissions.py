7952a0ed756a5a6b49b78986c77b7fca72fa8ebc
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2024-10-31 13:27:04 +0300

    permissions altered

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,6 +11,19 @@ class IsAdminOrReadOnly(BasePermission):
         return (request.method in SAFE_METHODS or (
             request.user.is_authenticated and request.user.is_admin))
 
+class IsAdminOrModeratorOrAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or (request.user.is_authenticated and (
+                    request.user.is_admin
+                    or request.user.is_staff
+                    or obj.author == request.user)))
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or (obj.author == request.user))
+
 class IsModeratorOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS or(
