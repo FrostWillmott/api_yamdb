f499fd59c37e21f42d6d42583cf42853e47f070f
Nikolai Volosiankov <wol87@ya.ru>
2024-10-31 21:05:04 +0100

    Improve IsAdminOrModeratorOrAuthorOrReadOnly for pass Postman test

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -13,13 +13,16 @@ class IsAdminOrReadOnly(BasePermission):
             request.user.is_authenticated and request.user.is_admin))
 
 class IsAdminOrModeratorOrAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-            or (request.user.is_authenticated and (
-                request.user.is_admin
-                or obj.author == request.user
-                or request.user.role == "moderator"))
-            )
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_admin
+            or obj.author == request.user
+            or request.user.role == "moderator"
+        )
 
 # class IsAuthorOrReadOnly(BasePermission):
 #     def has_object_permission(self, request, view, obj):
